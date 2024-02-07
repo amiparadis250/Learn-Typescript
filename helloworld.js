bed6eab9f1cff53c84d38d729b9afc8173e9d3cd
ISHIMWE Ami Paradis <141851169+amiparadis250@users.noreply.github.com>
2024-02-07 13:48:52 +0200

    typescript

--- helloworld.js
+++
@@ -1,6 +0,0 @@
-function Old(age: number): void {
-    console.log(age);
-}
-
-// Example usage
-Old(25); // Assuming 25 is the age you want to log
--- index.js
+++
@@ -1,6 +0,0 @@
-var sport = 'football';
-var id = 5;
-var names = "Ishimwe Ami Paradise";
-console.log(id);
-console.log(sport);
-console.log(names);
